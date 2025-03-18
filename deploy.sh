42f6c5ac1d1aa14379357fba8f3055dceb2f47dd
Tung Anh Vu <tung@iuuk.mff.cuni.cz>
2025-03-18 14:29:04 +0100

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+if [ "$HOSTNAME" != nikam -o "$USER" != null ] ; then
+    echo "Run me as stoc2025@nikam, please"
+    exit 1
+fi
+
+DEST=~/registration
+. $DEST/venv/bin/activate
+uv build
+uv pip install --force-reinstall dist/registration_app-0.1.0.tar.gz
+
+if [ -e $DEST/var/uwsgi.fifo ] ; then
+    echo "Reloading uwsgi"
+    echo r >$DEST/var/uwsgi.fifo || true
+else
+    echo "Restarting uwsgi"
+    systemctl --user restart registration_app
+fi
+
